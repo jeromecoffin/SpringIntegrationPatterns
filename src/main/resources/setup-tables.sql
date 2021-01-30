7934436003ca12c29fbcf8431ad9ef75b6cac280
jeromecoffin <jeromecoffin@yahoo.fr>
2021-01-30 13:45:10 +0100

    update var

--- src/main/resources/setup-tables.sql
+++ src/main/resources/setup-tables.sql
@@ -1,5 +1,5 @@
 create table IF NOT EXISTS USERS(USERNAME varchar(100),PASSWORD varchar(100), EMAIL varchar(100));
 create table IF NOT EXISTS Person(id integer identity primary key , name varchar(100));
-INSERT INTO USERS(USERNAME, PASSWORD, EMAIL) VALUES ('a', 'secret', 'spring-integration@awesome.com');
-INSERT INTO USERS(USERNAME, PASSWORD, EMAIL) VALUES ('b', 's3cr3t', 'spring@rocks.com');
-INSERT INTO USERS(USERNAME, PASSWORD, EMAIL) VALUES ('foo', 'bar', 'foo@bar.de');
+INSERT INTO USERS(USERNAME, PASSWORD, EMAIL) VALUES ('jerome', 'secret', 'jerome@efrei.com');
+INSERT INTO USERS(USERNAME, PASSWORD, EMAIL) VALUES ('camille', 's3cr3t', 'camille@efrei.com');
+INSERT INTO USERS(USERNAME, PASSWORD, EMAIL) VALUES ('gilles', 'password', 'gilles@efrei.com');
